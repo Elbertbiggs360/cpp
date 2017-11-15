720a36ac825f7e3844ddb2e6744a0b1f61f5e157
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2017-11-15 16:28:40 +0300

    Initial Commit

---
+++ mentorship/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mentorship
+//
+//  Created by Bruce Elbert Bigirwenkya on 15/11/2017.
+//  Copyright © 2017 Bruce Elbert Bigirwenkya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
