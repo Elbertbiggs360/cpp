0cd78c3b59a4e2087163aca8d4f03692b13cd813
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2017-11-20 18:25:00 +0300

    initial Commit

---
+++ mentorship/assn2/goto.c
@@ -0,0 +1,9 @@
+//
+//  goto.c
+//  mentorship
+//
+//  Created by Bruce Elbert Bigirwenkya on 20/11/2017.
+//  Copyright © 2017 Bruce Elbert Bigirwenkya. All rights reserved.
+//
+
+#include <stdio.h>
