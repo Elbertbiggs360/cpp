330f4308fc703bcd89488ab493d5c93d7c3ac889
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2017-12-06 10:42:02 +0300

    Use dynamic memory allocation to determine array size

---
+++ mentorship/week1/assn3/sort.c
@@ -0,0 +1,61 @@
+//
+//  sort.c
+//  mentorship
+//
+//  Created by Bruce Elbert Bigirwenkya on 06/12/2017.
+//  Copyright © 2017 Bruce Elbert Bigirwenkya. All rights reserved.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+int main(){
+    //Allows you to generate random number
+    srand(time(NULL));
+    
+    // Allows user to specify the original array size, stored in variable n1.
+    printf("Enter original array size:");
+    int n1=0;
+    scanf("%d",&n1);
+    
+    //Create a new array of n1 ints
+    int* a1 = (int*)calloc(n1, sizeof(int));/* Fill in*/
+    if (a1 == NULL) {
+        puts("Error: Allocation Failed!");
+        exit(EXIT_FAILURE);
+    }
+    int i;
+    for(i=0; i<n1; i++){
+        //Set each value in a1 to 100
+        a1[i]=100;
+        
+        //Print each element out (to make sure things look right)
+        printf("%d ", a1[i] );
+    }
+    
+    //User specifies the new array size, stored in variable n2.
+    printf("\nEnter new array size: ");
+    int n2=0;
+    scanf("%d",&n2);
+    
+    //Dynamically change the array to size n2
+    a1 = (int*)realloc(a1, n2*sizeof(int));
+    
+    //If the new array is a larger size, set all new members to 0. Reason: dont want to use uninitialized variables.
+    for(i=n1; i<n2+1; i++){
+        a1[i] = 0;
+    }
+    
+    for(i=0; i<n2;i++){
+        //Print each element out (to make sure things look right)
+        printf("%d ",a1[i]);
+    }
+    printf("\n");
+    free(a1);
+    
+    //Done with array now, done with program :D
+    
+    return 0;
+}
