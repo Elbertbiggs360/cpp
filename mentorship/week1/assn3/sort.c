ee1c15df7d38770e8631bb9e40f922310b74954a
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2017-12-06 12:34:03 +0300

    Fix Insertion Sort Use pointer addition to reference arrays and their elements Use malloc for dynamic memory allocation

--- mentorship/week1/assn3/sort.c
+++ mentorship/week1/assn3/sort.c
@@ -6,56 +6,63 @@
 //  Copyright © 2017 Bruce Elbert Bigirwenkya. All rights reserved.
 //
 
-#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
+//Sort an array A using insertion sort. Notice it is to be passed by reference.
+void sort(int* A, int n){
+    int tmp;
+    int i;
+    int j;
+    
+    for(i=1; i<n; i++){
+        tmp = *(A+i);
+        j=i-1;
+        //For each element in A, search for where it belong in the subarray preceeding it's current location
+        while(tmp<*(A+j) && j>=0){
+            *(A+j+1) = *(A+j);
+            j-=1;
+        }
+        *(A+j+1)=tmp;
+    }
+}
 
 int main(){
-    //Allows you to generate random number
+    //Allows use to generate random numbers
     srand(time(NULL));
     
-    // Allows user to specify the original array size, stored in variable n1.
-    printf("Enter original array size:");
-    int n1=0;
-    scanf("%d",&n1);
+    //Read a user input integer and store it in n
+    int n;
+    printf("Enter an integer n: ");
+    scanf("%d",&n);
     
-    //Create a new array of n1 ints
-    int* a1 = (int*)calloc(n1, sizeof(int));/* Fill in*/
-    if (a1 == NULL) {
-        puts("Error: Allocation Failed!");
-        exit(EXIT_FAILURE);
-    }
+    //Array array. Change this to become a dynamic array through malloc.
+    int *array = (int*)malloc(n*sizeof(int));
+    
+    //Assign each element in the array a random number between 0 and 31,999
     int i;
-    for(i=0; i<n1; i++){
-        //Set each value in a1 to 100
-        a1[i]=100;
-        
-        //Print each element out (to make sure things look right)
-        printf("%d ", a1[i] );
+    for (i=0; i<n; i++){
+        *(array+i)=rand()%32000; //This line assigns random numbers
     }
     
-    //User specifies the new array size, stored in variable n2.
-    printf("\nEnter new array size: ");
-    int n2=0;
-    scanf("%d",&n2);
-    
-    //Dynamically change the array to size n2
-    a1 = (int*)realloc(a1, n2*sizeof(int));
-    
-    //If the new array is a larger size, set all new members to 0. Reason: dont want to use uninitialized variables.
-    for(i=n1; i<n2+1; i++){
-        a1[i] = 0;
-    }
-    
-    for(i=0; i<n2;i++){
-        //Print each element out (to make sure things look right)
-        printf("%d ",a1[i]);
+    //Prints out the elements of the unsorted array
+    int x;
+    printf("The unsorted array is: ");
+    for (x=0; x<n; x++){
+        printf("%d ",*(array+x));
     }
     printf("\n");
-    free(a1);
     
-    //Done with array now, done with program :D
+    //Calls the sort function to sort the array
+    sort(array,n);
     
+    //Print out the elements of the now (supposedly) sorted array.
+    printf("The sorted array is: ");
+    for (x=0; x<n; x++){
+        printf("%d ",*(array+x));
+    }
+    printf("\n");
     return 0;
+    
 }
